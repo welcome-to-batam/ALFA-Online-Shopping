bf02084acf7a845635b4a3f680413d4ff321a50d
ALFA-Online-Shopping <cowokperenung@gmail.com>
2026-07-04 18:31:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alfa-cache-v31'; // Naikkan versi ke v31
+const CACHE_NAME = 'alfa-cache-v32'; // Naikkan versi ke v32
 const assets = [
   '/ALFA-Online-Shopping/',
   '/ALFA-Online-Shopping/index.html',
